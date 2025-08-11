dbdc90be780e8afd136845ea7fddb79f7db1e6c1
JadDavidIsReal <david.jad@icloud.com>
2025-08-11 17:39:15 +0800

    asdasd
    asdasd

--- src/pages/Inventory/History.tsx
+++ src/pages/Inventory/History.tsx
@@ -132,10 +132,6 @@ const History: React.FC = () => {
                                             <Briefcase className="w-5 h-5 text-white flex-shrink-0" />
                                             <p className="text-sm text-white ml-3 whitespace-nowrap">Employee</p>
                                         </div>
-                                        <div className="flex items-center p-2 hover:bg-[#77536A] rounded-lg cursor-pointer" onClick={() => handleNavigation('/search/community')}>
-                                            <Users className="w-5 h-5 text-white flex-shrink-0" />
-                                            <p className="text-sm text-white ml-3 whitespace-nowrap">Community</p>
-                                        </div>
                                     </div>
                                 )}
                             </div>
